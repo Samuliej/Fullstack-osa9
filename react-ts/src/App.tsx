68de6bb95f5bd76280d61e9463d805f2c4685264
Samuli Toppi <samuli.toppi@outlook.com>
2023-07-11 12:43:14 +0300

    Exercise 9.15, complete

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,12 +1,7 @@
+import { courseParts, CoursePart } from "./types";
+
 interface HeaderProps {
   courseName: string;
-}
-
-interface ContentProps {
-  courseParts: {
-    name: string,
-    exerciseCount: number
-  }[];
 }
 
 interface TotalProps {
@@ -15,20 +10,80 @@ interface TotalProps {
   }[];
 }
 
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+interface PartProps {
+  part: CoursePart;
+}
+
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+};
+
 const Header = (props: HeaderProps) => <h1>{props.courseName}</h1>;
+
+const Part = (props: PartProps) => {
+  switch (props.part.kind) {
+    case 'basic':
+      return (
+        <div>
+          <h3>{props.part.name} {props.part.exerciseCount}</h3>
+          <p><em>{props.part.description}</em></p>
+          <br />
+        </div>
+      );
+      break;
+    case 'group':
+      return (
+        <div>
+          <h3>{props.part.name} {props.part.exerciseCount}</h3>
+          <p>project exercises {props.part.groupProjectCount}</p>
+          <br />
+        </div>
+      )
+      break;
+    case 'background':
+      return (
+        <div>
+          <h3>{props.part.name} {props.part.exerciseCount}</h3>
+          <p><em>{props.part.description}</em></p>
+          <p>background material here: {props.part.backgroundMaterial}</p>
+          <br />
+        </div>
+      )
+      break;
+    case 'special':
+      return (
+        <div>
+          <h3>{props.part.name} {props.part.exerciseCount}</h3>
+          <p><em>{props.part.description}</em></p>
+          <p> required skills: {props.part.requirements.map(requirement => requirement + ', ')}</p>
+          <br />
+      </div>
+      )
+    default:
+      assertNever(props.part);
+      break;
+  }
+  return <div></div>;
+};
 
 const Content = (props: ContentProps) => {
   return (
-    <p>
-      {props.courseParts.map(part => <p key={part.name}>{part.name} {part.exerciseCount}</p>)}
-    </p>
+    <div>
+      {
+        props.courseParts.map(part => <Part key={part.name} part={part} />)
+      }
+    </div>
   );
 };
 
 const Total = (props: TotalProps) => {
   return (
     <p>
-      Number of exercises{" "}
+      Total number of exercises{" "}
       {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
     </p>
   );
@@ -36,20 +91,6 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
 
   return (
     <div>
