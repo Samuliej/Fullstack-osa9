40d5a1a1131ef7b51290e6ef998d9a7135c48235
Samuli Toppi <samuli.toppi@outlook.com>
2023-07-11 14:46:45 +0300

    Exercise 9.16, complete

---
+++ flight-diaries-front/src/App.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { Diary } from './types';
+import { getAllDiaries } from './diaryService';
+
+interface DiariesProps {
+  diaries: Diary[];
+}
+
+interface SingleDiaryProps {
+  diary: Diary;
+}
+
+const SingleDiary = (props: SingleDiaryProps) => {
+  return (
+    <div>
+      <h3>{props.diary.date}</h3>
+      <div key={props.diary.id}>
+        <p>Weather: {props.diary.weather}</p>
+        <p>Visibility: {props.diary.visibility}</p>
+        {props.diary.comment &&
+        <p>Comment: {props.diary.comment}</p>
+        }
+      </div>
+    </div>
+  )
+};
+
+const Diaries = (props: DiariesProps) => {
+
+
+  return (
+    <div>
+      <h2>Diary entries</h2>
+      {props.diaries.map(diary => <SingleDiary key={diary.id} diary={diary} />)}
+    </div>
+  )
+};
+
+function App() {
+  const [diaries, setDiaries] = useState<Diary[]>([]);
+
+  useEffect(() => {
+    getAllDiaries().then(data => {
+      setDiaries(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Diaries diaries={diaries} />
+    </div>
+  );
+}
+
+export default App;
